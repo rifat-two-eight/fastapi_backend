95ad89602e5096ab6cfd603c2b06e3decc3500bd
rifat-two-eight <rifat28.dev@gmail.com>
2026-06-04 17:27:41 +0600

    connected to database

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel,HttpUrl
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 app = FastAPI()
 
@@ -11,14 +14,28 @@ class Course(BaseModel):
     website: HttpUrl
 
 # database Postgresql
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost',database = 'postgres',user='postgres',password='1234',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('successfully connected database')
+        break
+    except Exception as error:
+        print("failed connected database")
+        print("Error",error)
+        time.sleep(2)
+
+
+@app.get("/")
+def root():
+    cursor.execute(""" SELECT * FROM course """)
+    data= cursor.fetchall()
+    return {"data":data}
 
 @app.post("/course")
 def create_post(post:Course):
     return {"data":post}
 
-@app.get("/")
-def root():
-    return {"name":"rifat"}
 
 @app.get("/details")
 def view_details():
